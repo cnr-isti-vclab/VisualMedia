9c3f22e18ef4b23e13c7a7516a6d990f9768cd55
Marco Callieri <callieri@isti.cnr.it>
2022-01-27 17:48:30 +0100

    navigation, first draft

--- wizard/navigation.php
+++ wizard/navigation.php
@@ -15,45 +15,36 @@
 		<iframe id="media" allowfullscreen allow="fullscreen" style="border-width:0px" class="relight" src="3d.php"></iframe>
 
 		<div class="panel">
-			<p>Skin:</p>
-		<?php foreach(array('dark', 'light', 'minimal_dark', 'minimal_light', 'transparent_dark', 'transparent_light') as $s) {?>
-			<img width="32" class="skins" data-skin="<?=$s?>" src="skins/<?=$s?>/home.png">
-		<?php } ?>
 
 		<hr/>
+		
+			<h5>
+			<img class="m-1" width="25px" src="skins/icons/restore.png" title="Reset Initial View" onclick="resetInitialView();"> Initial View
+			</h5>
+			<div class="m-1">
+			<center>
+				<button class="btn btn-primary btn-sm m-1" onclick="useCurrentView();">Use Current View</button>
+			</center>
+			</div>	
+			
+		<hr/>
 
-			<p>Background</p>
-			Colors: <input type="color" name="background-0" value="#aaaaaa"> 
-				    <input type="color" name="background-1" value="#000000"> <br/>
-			<input type="radio" name="background" value="flat"> Flat color <br/>
-			<input type="radio" name="background" value="linear" > Linear gradient <br/>
-			<input type="radio" name="background" value="radial"> Radial gradient <br/>
-
-			<div class="-group form-inline justify-content-between">
-				<div><input type="radio" name="background" value="image"> Image</div>
-				<select class="form-control  form-control-sm" name="image">
-					<option value="light.jpg">Light</option>
-					<option value="dark.jpg">Dark</option>
-				</select>
+			<h5>
+			<img class="m-1" width="25px" src="skins/icons/restore.png" title="Reset FOV" onclick="resetFOV();"> Field of View
+			</h5>
+			<div class="m-1">
+			<center>
+			<span class="h5 m-1" id="labelFOV">---</span>
+			<input type="range" min="5" max="85" step="1.0" id="rangeFOV" onInput="updatingFOV(this.value);" onChange="changedFOV(this.value);">
+			</br>
+			<button class="btn btn-primary btn-sm m-1" title="15°" onclick="changedFOV(15);">Tele</button>
+			<button class="btn btn-primary btn-sm m-1" title="35°" onclick="changedFOV(35);">Human</button>
+			<button class="btn btn-primary btn-sm m-1" title="80°" onclick="changedFOV(80);">FishEye</button>
+			</center>
 			</div>
-
+			
 		<hr/>
-
-			<p>Buttons:</p>
-			<input type="checkbox" name="tools[]" value="lighting"> <img src="skins/dark/lighting.png" width="24px"> Light on/off</br>
-			<input type="checkbox" name="tools[]" value="light"> <img src="skins/dark/light.png" width="24px"> Light direction</br>
-			<input type="checkbox" name="tools[]" value="color"> <img src="skins/dark/color.png" width="24px"> Color</br>
-			<input type="checkbox" name="tools[]" value="measure"> <img src="skins/dark/measure.png" width="24px"> Measure</br>
-			<input type="checkbox" name="tools[]" value="pick"> <img src="skins/dark/pick.png" width="24px"> Picking</br>
-			<input type="checkbox" name="tools[]" value="sections"> <img src="skins/dark/sections.png" width="24px"> Sections</br>
-			<input type="checkbox" name="tools[]" value="orthographic"> <img src="skins/dark/orthographic.png" width="24px"> Orthographic</br>
-
-		<hr/>
-
-			<div class="row">
-				<div class="col-6"><button class="btn btn-secondary btn-sm btn-block" name="reset"> Reset everything </button></div>
-			</div>
-
+		
 		</div>
 
 	</div>
@@ -62,79 +53,77 @@
 <script src="config.js"></script>
 <script>
 
-class Look extends Config {
+class Navigation extends Config {
 	constructor(frame, options) {
 		super(frame, options);
 
-		this.types = document.querySelectorAll('input[name=background]');
-		this.image = document.querySelector('select[name=image]');
-		this.color0 = document.querySelector('input[type=color][name="background-0"]');
-		this.color1 = document.querySelector('input[type=color][name="background-1"]');
-		this.skins = document.querySelectorAll('.skins');
-		this.tools = [...document.querySelectorAll('input[name="tools[]"')];
 
-		for(let type of this.types) 
-			type.addEventListener('change', () => {
-				if(type.checked) {
-					this.options.background.type = type.value;
-					this.save();
-				}
-			});
-
-		for(let property of ['image', 'color0', 'color1']) {
-			this[property].addEventListener('change', () => {
-				this.options.background[property] = this[property].value;
-				this.save();
-			});
-		}
-
-		for(let skin of this.skins) {
-			let value = skin.getAttribute('data-skin');
-			skin.addEventListener('click', (e) => this.set('skin', value))
-		}
-
-		for(let tool of this.tools) {
-			let value = tool.value;
-			tool.addEventListener('change', 
-				() => { this.set('tools',
-					this.tools.filter(t => t.checked).map(i => i.value));
-				}
-			);
-
-		}
 	}
 
 	update() {
 		let options = this.options;
-
-		let background = options.background || 'flat';
-		let type = background.type;
-		for(let input of this.types)
-			input.checked = input.value == type;
-
-		this.image.value = background.value;
-		this.color0.value = background.color0;
-		this.color1.value = background.color1;
-
-		for(let tool of this.tools) {
-			if(options.tools.includes(tool.value))
-				tool.checked = true;
-		}
+		
+		//fov
+		document.querySelector('#rangeFOV').value = options.space.cameraFOV;
+		document.querySelector('#labelFOV').innerHTML = options.space.cameraFOV + "°";
 	}
+	
 	reset() {
 		super.reset();
 		this.update();
 	}
 }
 
+//----------------------------------------------------------------------------------
 
+function updatingFOV(newVal){
+	document.querySelector('#labelFOV').innerHTML = newVal + "°";
+	window.frames[0].presenter._scene.space.cameraFOV = newVal;
+	window.frames[0].presenter.repaint();	
+}
+function changedFOV(newVal){
+	navigation.options.space.cameraFOV = newVal;
+	navigation.save();
+	navigation.update();	
+}
+function resetFOV(){
+	navigation.options.space.cameraFOV = default_ariadne.space.cameraFOV;
+	navigation.save();
+	navigation.update();
+}
+//----------------------------------------------------------------------------------
 
+function useCurrentView(){
+	
+	var track = window.frames[0].presenter.getTrackballPosition();
+	
+	navigation.options.trackball.trackOptions.startPhi      = track[0];
+	navigation.options.trackball.trackOptions.startTheta    = track[1];
+	navigation.options.trackball.trackOptions.startPanX     = track[2];
+	navigation.options.trackball.trackOptions.startPany     = track[3];
+	navigation.options.trackball.trackOptions.startPanZ     = track[4];
+	navigation.options.trackball.trackOptions.startDistance = track[5];
+	
+	navigation.save();
+	navigation.update();	
+}
+function resetInitialView(){
+	navigation.options.trackball.trackOptions.startPhi      = default_ariadne.trackball.trackOptions.startPhi;
+	navigation.options.trackball.trackOptions.startTheta    = default_ariadne.trackball.trackOptions.startTheta;
+	navigation.options.trackball.trackOptions.startPanX     = default_ariadne.trackball.trackOptions.startPanX;
+	navigation.options.trackball.trackOptions.startPany     = default_ariadne.trackball.trackOptions.startPany;
+	navigation.options.trackball.trackOptions.startPanZ     = default_ariadne.trackball.trackOptions.startPanZ;
+	navigation.options.trackball.trackOptions.startDistance = default_ariadne.trackball.trackOptions.startDistance;
 
-let look = new Look('#media', 'update.php'); //'options.json'); 
+	navigation.save();
+	navigation.update();	
+}
+
+let navigation = new Navigation('#media', 'update.php'); //'options.json'); 
 
 
 let reset = document.querySelector('button[name=reset]');
-reset.addEventListener('click', () => look.reset());
+reset.addEventListener('click', () => navigation.reset());
 
 
 
