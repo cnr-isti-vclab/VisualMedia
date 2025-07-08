15f4c94a9c078b0b3a7354d8a3e60222b5bb4c1d
alemuntoni <alemuntoni@users.noreply.github.com>
2025-07-08 14:37:20 +0200

    mesh processing python functions - to be called from ariadne.py

---
+++ docker-python/processing.py
@@ -0,0 +1,52 @@
+import pymeshlab
+
+def simplify(input_file, output_file, target_face_num):
+    """
+    Simplifies a mesh using PyMeshLab.
+
+    :param input_file: Path to the input mesh file.
+    :param output_file: Path to save the simplified mesh.
+    :param target_face_num: Target number of faces for the simplified mesh.
+    """
+    ms = pymeshlab.MeshSet()
+    ms.load_new_mesh(input_file)
+    
+    # Apply simplification
+    ms.meshing_decimation_quadric_edge_collapse(targetfacenum=target_face_num)
+    
+    # Save the simplified mesh
+    ms.save_current_mesh(output_file)
+
+
+def remesh(input_file, output_file, n_iterations=10): # size of triangles??
+    """
+    Remeshes a mesh using PyMeshLab.
+
+    :param input_file: Path to the input mesh file.
+    :param output_file: Path to save the remeshed mesh.
+    """
+    ms = pymeshlab.MeshSet()
+    ms.load_new_mesh(input_file)
+    
+    # Apply remeshing
+    ms.meshing_isotropic_explicit_remeshing(iterations=n_iterations)
+    
+    # Save the remeshed mesh
+    ms.save_current_mesh(output_file)
+
+
+def close_holes(input_file, output_file): # size of holes??
+    """
+    Closes holes in a mesh using PyMeshLab.
+
+    :param input_file: Path to the input mesh file.
+    :param output_file: Path to save the mesh with closed holes.
+    """
+    ms = pymeshlab.MeshSet()
+    ms.load_new_mesh(input_file)
+    
+    # Apply hole closing
+    ms.meshing_close_holes()
+    
+    # Save the mesh with closed holes
+    ms.save_current_mesh(output_file)
